b53245c369de1d049d54a5b9875d9d771fff7c41
Pavel Prorovsky <prorovsky@gmail.com>
2017-01-18 21:26:57 +0300

    add login and registration

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let express = require('express'),
     User = require('./models/user'),
     Restaurant = require('./models/restaurant'),
     Comment = require('./models/comment'),
+    config = require('./config'),
     dburl = process.env.DATABASEURL || 'mongodb://localhost/restaurants-guide',
     seedDB = require('./seeds'),
     app = express();
@@ -21,13 +22,26 @@ mongoose.connect(dburl, function(err, db){
     }
 });
 
+// passport config
 app.use(require('express-session')({
-    secret: 
+    secret: process.env.SECRET || config.secret,
+    resave: false,
+    saveUninitialized: false
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 // routes
 app.get('/', function(req, res){
@@ -69,7 +83,7 @@ app.get('/restaurants/:id', function(req, res){
 });
 
 // comments routes
-app.get('/restaurants/:id/comments/new', function(req, res){
+app.get('/restaurants/:id/comments/new', isLoggedIn, function(req, res){
     Restaurant.findById(req.params.id, function(err, restaurant){
         if(err){
             console.error(err);
@@ -79,7 +93,7 @@ app.get('/restaurants/:id/comments/new', function(req, res){
     });
 });
 
-app.post('/restaurants/:id/comments', function(req, res){
+app.post('/restaurants/:id/comments', isLoggedIn, function(req, res){
     Restaurant.findById(req.params.id, function(err, restaurant){
         if(err){
             console.log(err);
@@ -97,6 +111,47 @@ app.post('/restaurants/:id/comments', function(req, res){
     });
 });
 
+// authenticate
+app.get('/register', function(req, res){
+    res.render('register');
+});
+
+app.post('/register', function(req, res){
+    let newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.error(err);
+            return res.render('register');
+        }
+        passport.authenticate('local')(req, res, function(){
+            res.redirect('/restaurants');
+        });
+    });
+});
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.post('/login', passport.authenticate('local',
+    {
+        successRedirect: '/restaurants',
+        failureRedirect: '/login'
+}));
+
+//logic route
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/restaurants');
+});
+
 app.listen(process.env.PORT || 3000, function(){
     console.log('Server started...');
 });
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
